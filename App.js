f8902f33cf01c97a079e019a42b2fdb8150c38ca
Leo <leo.pan.duh@gmail.com>
2019-05-28 19:25:00 -0400

    add space for android status bar and fix alignment of main container

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import React from "react";
-import { Button, Image, StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  Image,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World</Text>
+      <View style={styles.rootContainer}>
+        <View style={styles.androidStatusBar} />
+        <View style={styles.container}>
+          <Text>Hello World</Text>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  androidStatusBar: {
+    backgroundColor: "black",
+    height: StatusBar.currentHeight
+  },
+  rootContainer: {
+    flex: 1
+  },
   container: {
     flex: 1,
+    paddingTop: Platform.OS === "ios" ? 0 : StatusBar.currentHeight,
     backgroundColor: "#fff",
+    flexDirection: "row",
     alignItems: "center",
     justifyContent: "center"
   }
