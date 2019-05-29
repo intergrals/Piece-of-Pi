99359dd97d52305cde157a169ae4f13187150ec7
Leo <leo.pan.duh@gmail.com>
2019-05-29 14:00:36 -0400

    move handling of pi to App so it can be stored as state rather than js vars

--- App.js
+++ App.js
@@ -9,16 +9,41 @@ import {
   View
 } from "react-native";
 import PiPage from "./src/PiPage";
+import strings from "./res/strings";
 
 export default class App extends React.Component {
+  state = {
+    pi: strings.pi.slice(0, 1000),
+    chunkSize: 4,
+    offset: 0,
+    count: 0
+  };
+
+  // reset count
+  reset = () => {
+    this.setState({
+      count: 0
+    });
+  };
+
+  // get the next chunk of pi
+  getChunk = () => {
+    // second digit is '1', not '.'
+    piOffset = this.state.offset === 0 ? 0 : this.state.offset + 1;
+    // get an additional character if offset is 0 and first time fetching
+    getAdditional = this.state.offset === 0 && this.state.count === 0 ? 1 : 0;
+    start = piOffset + this.state.chunkSize * this.state.count++;
+    return this.state.pi.slice(
+      start,
+      start + this.state.chunkSize + getAdditional
+    );
+  };
+
   render() {
     return (
       <View style={styles.rootContainer}>
         <View style={styles.androidStatusBar} />
-        {/* <View style={styles.container}>
-          <Text>Hello World</Text>
-        </View> */}
-        <PiPage />
+        <PiPage getChunk={this.getChunk} style={styles.container} />
       </View>
     );
   }
